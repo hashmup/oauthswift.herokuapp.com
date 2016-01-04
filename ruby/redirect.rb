82e4bcaa4f0bb11fcba7b4185a2012ef240e4388
hashmup <hashmup@gmail.com>
2016-01-04 13:54:39 -0600

    Update redirect.rb

--- ruby/redirect.rb
+++ ruby/redirect.rb
@@ -1,19 +1,9 @@
 # encoding: utf-8
-require 'logger'
-Dir.mkdir('logs') unless File.exist?('logs')
-$log = Logger.new('logs/output.log','weekly')
 
-configure :production do
-  $log.level = Logger::WARN
-end
-configure :development do
-  $log.level = Logger::DEBUG
-end
 get "/callback/*" do
   path = request.path.gsub("/callback","")
   path_query = "#{path}?#{request.query_string}"
   url = "perobo://oauth-callback#{path_query}"  
   puts url
-  $log.debug url
   redirect url
 end
