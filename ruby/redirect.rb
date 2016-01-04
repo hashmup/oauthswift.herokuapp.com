bdd4967a566a581c37e6c79bc4947d2371980595
hashmup <hashmup@gmail.com>
2016-01-04 03:05:12 -0600

    Update redirect.rb

--- ruby/redirect.rb
+++ ruby/redirect.rb
@@ -5,12 +5,5 @@ get "/callback/*" do
   path_query = "#{path}?#{request.query_string}"
   url = "perobo://oauth-callback#{path_query}"  
   puts url
-  error_logger = Logger.new('log/errors.logrror do
-    e = request.env['sinatra.error']
-    error_logger.info "#{url}"
-    Kernel.puts "#{url}"
-    puts url
-    'Application error'
-  end
   redirect url
 end
