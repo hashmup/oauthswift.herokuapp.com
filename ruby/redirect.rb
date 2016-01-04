72029e0785680afc7700a7651c20ddf0fd018e3e
hashmup <hashmup@gmail.com>
2016-01-04 14:00:04 -0600

    Update redirect.rb

--- ruby/redirect.rb
+++ ruby/redirect.rb
@@ -4,6 +4,7 @@ get "/callback/*" do
   path = request.path.gsub("/callback","")
   path_query = "#{path}?#{request.query_string}"
   url = "perobo://oauth-callback#{path_query}"  
+  puts "hello"
   puts "#{url}"
   redirect url
 end
