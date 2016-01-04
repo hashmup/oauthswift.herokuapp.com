6417350b3d54932d34ac626fcc107b59525bb54f
hashmup <hashmup@gmail.com>
2016-01-04 02:52:47 -0600

    Update redirect.rb

--- ruby/redirect.rb
+++ ruby/redirect.rb
@@ -1,10 +1,19 @@
 # encoding: utf-8
+require 'sinatra'
+
+configure do
+  # logging is enabled by default in classic style applications,
+  # so `enable :logging` is not needed
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
 
 get "/callback/*" do
   path = request.path.gsub("/callback","")
   path_query = "#{path}?#{request.query_string}"
   url = "perobo://oauth-callback#{path_query}"  
   puts url
-  Rails.logger.debug url
+  logger.info url
   redirect url
 end
