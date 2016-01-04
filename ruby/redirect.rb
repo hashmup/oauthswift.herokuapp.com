492ce0bec57fa515e4b108a8bb5505a85aaa0ab6
hashmup <hashmup@gmail.com>
2016-01-04 14:09:02 -0600

    Update redirect.rb

--- ruby/redirect.rb
+++ ruby/redirect.rb
@@ -1,5 +1,11 @@
 # encoding: utf-8
+require 'logger'
 
+enable :logging
+
+before do
+  logger.level = Logger::DEBUG
+end
 get "/callback/*" do
   path = request.path.gsub("/callback","")
   path_query = "#{path}?#{request.query_string}"
