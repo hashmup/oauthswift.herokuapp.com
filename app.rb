75afb10edb91ce976794f5669ed29de0123dbb72
hashmup <hashmup@gmail.com>
2016-01-04 14:14:55 -0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -2,12 +2,6 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'logger'
 
-enable :logging
-
-before do
-  logger.level = Logger::DEBUG
-end
 load 'ruby/redirect.rb'
 load 'ruby/index.rb'
