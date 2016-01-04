e9e3f3ed0c52ed19dc94a9f202e0f55d9b1ed266
hashmup <hashmup@gmail.com>
2016-01-04 15:06:36 -0600

    Update redirect.rb

--- ruby/redirect.rb
+++ ruby/redirect.rb
@@ -1,18 +1,9 @@
 # encoding: utf-8
-require 'logger'
 
-enable :logging
-
-before do
-  logger.level = Logger::DEBUG
-end
 get "/callback/*" do
   path = request.path.gsub("/callback","")
   path_query = "#{path}?#{request.query_string}"
   url = "perobo://oauth-callback#{path_query}"  
-  puts "hello"
-  logger.info url
-  logger.debug url
-  return url
+  puts url
   redirect url
 end
